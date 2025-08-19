63b481da5a5ac088db9f4c63707ae9485b244a80
olinkirkland <olin.kirkland@beg.de>
2025-08-19 15:48:01 +0200

    Adjust dependencies, add Redis URL variable

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,13 @@
+import dotenv from 'dotenv';
+
 (async () => {
+    dotenv.config();
     console.log('Starting server...');
-    // await redisBroker.start();
-    // await restServer.start();
-    // broadcaster.start();
-    // systemInfoController.start();
+    // Connect to Redis database (Railway deployment)
+    // Need: env variable REDIS_URL, e.g. redis://default:password@host:port
+    const redisUrl = process.env.REDIS_URL;
+    if (!redisUrl) {
+        console.error('REDIS_URL environment variable is not set.');
+        process.exit(1);
+    }
 })();
