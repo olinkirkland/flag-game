7836141146b5e9c06e603002b4982f0ed813586e
Olin Kirkland <me@olinkirk.land>
2025-09-21 14:15:27 -0400

    Update cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,12 @@ import gameRouter from './routes/game-router';
 
 export function createServer() {
     const app = express();
-    app.use(cors());
+    app.use(
+        cors({
+            origin: '*',
+            methods: ['GET', 'POST', 'PUT', 'DELETE']
+        })
+    );
     app.use(express.json());
 
     app.use('/api/user', userRouter);
